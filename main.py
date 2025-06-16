6c1b6a04c2fc7759ee731212f995a001f2cacf38
bedrohenr <ferfonhp@gmail.com>
2025-06-16 00:00:46 -0300

    refactor: adicionando ambos repositorios

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from src.assets.aux.flags import flags
 
 # Todos seus repositorios:  ["username/*"] ou 
 # Repositorios especificos: ["username/repo1", "username/repo2"] ...
-repos = ["bpthiago/oraculo"]
+repos = ["bpthiago/oraculo-documentation", "bpthiago/oraculo"]
 
 # Quais informações deseja trazer do github
 streams = ["issues", "repositories", "pull_requests", "commits", "teams", "users", "issue_milestones", "projects_v2", "team_members", "team_memberships"]
