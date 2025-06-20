54dc040d8d365287573bb691948cd6a151f0f7c1
Pedro Ferreira <ferfonhp@gmail.com>
2025-06-20 17:07:10 -0300

    fix: agora é possível conectar a api pela porta 8000
    por algum motivo 127.0.0.1 (default) não funcionava

--- main.py
+++ main.py
@@ -27,6 +27,6 @@ if(flags.etl == True or flags.etl_only == True):
 
 api_root_path = "src.api.app"
 port = 8000
-config = uvicorn.Config(api_root_path + ":app", port=port, log_level="info", reload=True)
+config = uvicorn.Config(api_root_path + ":app",host="0.0.0.0", port=port, log_level="info", reload=True)
 server = uvicorn.Server(config)
 server.run()
